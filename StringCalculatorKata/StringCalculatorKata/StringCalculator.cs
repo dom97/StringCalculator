bee04959ebeadc07b6703d405afa140f9d4d32e3
Dom Edwards <dedwards@qualtrax.com>
2016-04-18 12:13:38 -0400

    initial commit

---
+++ StringCalculatorKata/StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        public Int32 Add(String inputNumbers)
+        {
+            ValidateInputForNegativeNumbers(inputNumbers);
+            var sum = CalculateSum(inputNumbers);
+
+            return sum;
+        }
+
+        private Int32 CalculateSum(String inputNumbers)
+        {
+            var nonNegativeDigitsPattern = @"[^\d]";
+            var positiveNumbers = Regex.Split(inputNumbers, nonNegativeDigitsPattern);
+            var sum = 0;
+
+            foreach (var number in positiveNumbers)
+                sum += GetNumberToAdd(number);
+
+            return sum;
+        }
+
+        private Int32 GetNumberToAdd(String number)
+        {
+            var value = 0;
+
+            if (!String.IsNullOrEmpty(number))
+            {
+                var numberValue = Int32.Parse(number);
+
+                if (numberValue <= 1000)
+                    value = numberValue;
+            }
+
+            return value;
+        }
+
+        private void ValidateInputForNegativeNumbers(String inputNumbers)
+        {
+            var negativeIntegersPattern = @"[\-][\d]";
+            var negativeNumberMatches = Regex.Matches(inputNumbers, negativeIntegersPattern);
+
+            if (negativeNumberMatches.Count > 0)
+            {
+                var negativeNumbers = negativeNumberMatches.Cast<Match>().Select(m => m.Value).ToArray();
+
+                throw new Exception("Negatives not allowed: " + String.Join(", ", negativeNumbers));
+            }
+        }
+    }
+}
---
+++ StringCalculatorKata/UnitTestProject1/CalculatorTests.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculatorKata;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        StringCalculator calculator;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            calculator = new StringCalculator();
+        }
+
+        [TestMethod]
+        public void VerifyThatIntegerIsReturned()
+        {
+            var result = calculator.Add("0");
+
+            Assert.IsInstanceOfType(result, typeof(Int32));
+        }
+
+        [TestMethod]
+        public void PassingEmptyStringReturnsZero()
+        {
+            var result = calculator.Add(String.Empty);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void PassingASingleIntegerReturnsThatInteger()
+        {
+            var result = calculator.Add("1");
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void PassingTwoIntegersWhichAreCommaDelimetedReturnsTheSumOfThoseIntegers()
+        {
+            var result = calculator.Add("1,2");
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void PassingMultipleIntegersReturnsTheSumOfThoseIntegers()
+        {
+            var result = calculator.Add("1,2,1,2");
+
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void PassingIntegersDelimetedByNewLinesReturnsTheSumOfThoseIntegers()
+        {
+            var numbers = String.Format("1{0}2", Environment.NewLine);
+            var result = calculator.Add(numbers);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void UsingAlternateDelimeterReturnsSumOfIntegers()
+        {
+            var numbers = String.Format("//;{0}1;2", Environment.NewLine);
+            var result = calculator.Add(numbers);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void PassingASingleNegateiveNumberWithoutADelimeterThrowsAnExceptionListingThatNumber()
+        {
+            var numbers = String.Format("-1", Environment.NewLine);
+
+            try
+            {
+                calculator.Add(numbers);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(ex.Message, "Negatives not allowed: -1");
+            }
+        }
+
+        [TestMethod]
+        public void PassingAPositiveIntegerFollowedByANegateiveIntegerThrowsAnExceptionListingThatNumber()
+        {
+            var numbers = String.Format("//;{0}1;-2", Environment.NewLine);
+
+            try
+            {
+                calculator.Add(numbers);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(ex.Message, "Negatives not allowed: -2");
+            }
+        }
+
+        [TestMethod]
+        public void PassingANegativeIntegerFollowedByAPositiveIntegerThrowsAnExceptionListingThatNumber()
+        {
+            var numbers = String.Format("//;{0}-1;2", Environment.NewLine);
+
+            try
+            {
+                calculator.Add(numbers);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(ex.Message, "Negatives not allowed: -1");
+            }
+        }
+
+        [TestMethod]
+        public void PassingMultipleNegativeIntegersThrowsAnExceptionListingTheNumbers()
+        {
+            var numbers = String.Format("//;{0}-1;-2", Environment.NewLine);
+
+            try
+            {
+                calculator.Add(numbers);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(ex.Message, "Negatives not allowed: -1, -2");
+            }
+        }
+
+        [TestMethod]
+        public void PassingASingleIntegerLargerThanOneThousandIsIgnored()
+        {
+            var numbers = "1001";
+            var result = calculator.Add(numbers);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void AMultipleIntegersWithOneBeingLargerThanOneThousandReturnsAResultExcludingThatInteger()
+        {
+            var numbers = "1001,2,3";
+            var result = calculator.Add(numbers);
+
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void PassingADelimeterOfMultipleCharactersReturnsProperSum()
+        {
+            var numbers = String.Format("//[***]{0}1***2***3", Environment.NewLine);
+            var result = calculator.Add(numbers);
+
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void PassingMultipleDelimetersReturnsProperSum()
+        {
+            var numbers = String.Format("//[*][%]{0}1*2%3", Environment.NewLine);
+            var result = calculator.Add(numbers);
+
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void PassingMultipleDelimetersWhichAreLongerThanOneCharactersReturnsProperSum()
+        {
+            var numbers = String.Format("//[**][%%]{0}1**2%%3", Environment.NewLine);
+            var result = calculator.Add(numbers);
+
+            Assert.AreEqual(6, result);
+        }
+    }
+}
